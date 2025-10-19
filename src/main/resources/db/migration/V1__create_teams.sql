3c93064bb8673401f54ee3ec03a8e233a440193b
SebastianRamos3 <seramos@csumb.edu>
2025-10-19 13:20:24 -0700

    populating the database

--- src/main/resources/db/migration/V1__create_teams.sql
+++ src/main/resources/db/migration/V1__create_teams.sql
@@ -6,9 +6,3 @@ CREATE TABLE IF NOT EXISTS nba_team (
     nba_franchise BOOLEAN NOT NULL
 );
 
-INSERT INTO nba_team (name, nickname, logo, nba_franchise) VALUES
-('Los Angeles Lakers', 'Lakers', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Los_Angeles_Lakers_logo.svg/1200px-Los_Angeles_Lakers_logo.svg.png', TRUE),
-('Golden State Warriors', 'Warriors', 'https://upload.wikimedia.org/wikipedia/en/thumb/0/01/Golden_State_Warriors_logo.svg/1200px-Golden_State_Warriors_logo.svg.png', TRUE),
-('Boston Celtics', 'Celtics', 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Boston_Celtics.svg/1200px-Boston_Celtics.svg.png', TRUE),
-('Chicago Bulls', 'Bulls', 'https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Chicago_Bulls_logo.svg/1200px-Chicago_Bulls_logo.svg.png', TRUE),
-('Miami Heat', 'Heat', 'https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Miami_Heat_logo.svg/1200px-Miami_Heat_logo.svg.png', TRUE);
