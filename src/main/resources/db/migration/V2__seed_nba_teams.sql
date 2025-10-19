3e0aed31a67c49adbd005b5a9fb732dfa86d864d
SebastianRamos3 <seramos@csumb.edu>
2025-10-19 13:31:27 -0700

    redploying again

--- src/main/resources/db/migration/V2__seed_nba_teams.sql
+++ src/main/resources/db/migration/V2__seed_nba_teams.sql
@@ -4,3 +4,4 @@ INSERT INTO nba_team (name, nickname, logo, nba_franchise) VALUES
 ('Boston Celtics', 'Celtics', 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Boston_Celtics.svg/1200px-Boston_Celtics.svg.png', TRUE),
 ('Chicago Bulls', 'Bulls', 'https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Chicago_Bulls_logo.svg/1200px-Chicago_Bulls_logo.svg.png', TRUE),
 ('Miami Heat', 'Heat', 'https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Miami_Heat_logo.svg/1200px-Miami_Heat_logo.svg.png', TRUE);
+//reploying the application
