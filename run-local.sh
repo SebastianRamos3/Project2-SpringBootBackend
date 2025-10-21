5d875ef09a3f658a45ce8646fc324a4fe16df7f5
SebastianRamos3 <seramos@csumb.edu>
2025-10-21 12:58:53 -0700

    oops

--- run-local.sh
+++
@@ -1,6 +0,0 @@
-
-export JAWSDB_URL="jdbc:mysql://h7xe2knj2qb6kxal.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bmsx53vz3sq1a65q"
-export JAWSDB_USERNAME="m68u8ktlm1jk9a3m"
-export JAWSDB_PASSWORD="vmskdlbpn96mu5ew"
-./mvnw spring-boot:run
-
