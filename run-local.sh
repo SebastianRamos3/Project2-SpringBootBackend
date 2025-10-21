422c3d01efda118945ed93e0ea07adc04c261598
SebastianRamos3 <seramos@csumb.edu>
2025-10-21 12:46:53 -0700

    Remove game controller and application.properties, clean build artifacts

---
+++ run-local.sh
@@ -0,0 +1,6 @@
+
+export JAWSDB_URL="jdbc:mysql://h7xe2knj2qb6kxal.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bmsx53vz3sq1a65q"
+export JAWSDB_USERNAME="m68u8ktlm1jk9a3m"
+export JAWSDB_PASSWORD="vmskdlbpn96mu5ew"
+./mvnw spring-boot:run
+
